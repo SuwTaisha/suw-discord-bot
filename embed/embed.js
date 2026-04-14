3bf53dafc0c7b661a5e49bb1e5dde07e26664c67
Suwa <hoangmen0715@gmail.com>
2026-04-14 15:36:00 +0700

    Changed embed variable's name

--- embed/embed.js
+++ embed/embed.js
@@ -2,14 +2,14 @@ const { EmbedBuilder } = require("discord.js")
 
 
 function embedBuild(title, description, image, footerText, footerUrl) {
-    const exampleEmbed = new EmbedBuilder()
+    const embed = new EmbedBuilder()
         .setColor(0x0099ff)
         .setTitle(`${title}`)
         .setDescription(`${description}`)
         .setImage(`${image}`)
         .setTimestamp()
         .setFooter({ text: `${footerText}`, iconURL: `${footerUrl}` })
-        return exampleEmbed;
+        return embed;
 }
 
 module.exports = embedBuild
